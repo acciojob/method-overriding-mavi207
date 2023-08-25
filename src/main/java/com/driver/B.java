a10a31d75802ec438eef9e20b9960c7a52a2e5bb
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-08-26 02:12:24 +0530

    Completed

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,6 @@
 package com.driver;
 
 class B extends A {
-
     @Override
     String meth() {
         return "Method is overridden in Extended class B";
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,8 @@
 package com.driver;
 
 public class Main {
-
-    public static void main (String [] args){
+    public static void main(String[] args) {
         B objB = new B();
-        objB.meth();
-        objB.meth();
+        System.out.println(objB.meth());  // This will now call the overridden meth() from class B
     }
 }
