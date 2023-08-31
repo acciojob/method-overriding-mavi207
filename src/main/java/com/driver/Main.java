a65cd627f6f46e7c4c6a8991c57730ee6fe50e89
Mohammad Maviya Waseem Shaikh <maviya9830@gmail.com>
2023-09-01 03:29:09 +0530

    submit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -26,6 +26,6 @@ class B extends A {
     // Task 4: Override method meth in class B
     @Override
     public String meth() {
-        return "Method is overridden in Extendend class B";
+        return "Method is overridden in Extended class B";
     }
 }
